8bbb9d68c4ff90e5f863eb2d73622250595416f7
简佳 <15779599039@163.com>
2023-06-19 10:27:52 +0800

    更新wr30u配置

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -4,22 +4,25 @@
 git clone --depth=1 --single-branch --branch $(echo $openWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
-git clone --depth=1 --single-branch --branch $(echo $openWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#git clone --depth=1 --single-branch --branch $(echo $openWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+#git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 #Linkease
-git clone --depth=1 --single-branch https://github.com/linkease/istore.git
-git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
-git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Open Clash
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 #Pass Wall
 #git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
 #git clone --depth=1 --single-branch --branch "packages" https://github.com/xiaorouji/openwrt-passwall.git ./pw_packages
 #unblockneteasemusic
-rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-git clone --depth=1 --single-branch --branch "master" https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+#rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+#git clone --depth=1 --single-branch --branch "master" https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
 #xlnetacc
-git clone --depth=1 --single-branch --branch "master" https://github.com/a7909a/luci-app-xlnetacc.git
+#git clone --depth=1 --single-branch --branch "master" https://github.com/a7909a/luci-app-xlnetacc.git
+
+#adguardhome
+git clone --depth=1 --single-branch --branch "master" https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
