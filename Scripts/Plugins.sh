529634ebf7a976c39bf168b67b664264deeb31dd
简佳 <15779599039@163.com>
2023-09-08 22:39:21 +0800

    更新

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -40,6 +40,8 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+#Open Clash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
