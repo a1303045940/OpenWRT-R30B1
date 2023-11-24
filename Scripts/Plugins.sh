e98b659b650adf05f1887c01930febdbfb8b14f2
简佳 <15779599039@163.com>
2023-11-24 11:00:05 +0800

    更新

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -40,37 +40,37 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-#Open Clash
-git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+# #Open Clash
+# git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 
-#预置OpenClash内核和GEO数据
-export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+# #预置OpenClash内核和GEO数据
+# export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+# export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+# export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+# export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
 
-export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+# export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+# export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
-export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
-export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
-export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+# export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+# export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+# export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
 
-cd ./OpenClash/luci-app-openclash/root/etc/openclash
+# cd ./OpenClash/luci-app-openclash/root/etc/openclash
 
-curl -sfL -o ./Country.mmdb $GEO_MMDB
-curl -sfL -o ./GeoSite.dat $GEO_SITE
-curl -sfL -o ./GeoIP.dat $GEO_IP
+# curl -sfL -o ./Country.mmdb $GEO_MMDB
+# curl -sfL -o ./GeoSite.dat $GEO_SITE
+# curl -sfL -o ./GeoIP.dat $GEO_IP
 
-mkdir ./core && cd ./core
+# mkdir ./core && cd ./core
 
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz && mv ./tun ./clash_tun
+# curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
+# gzip -d ./tun.gz && mv ./tun ./clash_tun
 
-curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+# curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+# tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
 
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-tar -zxf ./dev.tar.gz
+# curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
+# tar -zxf ./dev.tar.gz
 
-chmod +x ./clash* ; rm -rf ./*.gz
+# chmod +x ./clash* ; rm -rf ./*.gz
