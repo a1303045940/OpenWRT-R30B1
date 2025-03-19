fb5f12ef2cfb936295ba8c3b40eaf2fcc3685771
JianJia2018 <39438074+JianJia2018@users.noreply.github.com>
2025-03-19 19:17:21 +0800

    更新

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -31,3 +31,9 @@ sed -i 's:/bin/ash:/bin/bash:g' /etc/passwd
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 ./scripts/feeds update -a && ./scripts/feeds install -a
+
+# 注释原行（精确匹配原URL和版本）
+sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23.05/s/^/#/' "feeds.conf.default"
+
+# 添加新行到文件末尾
+echo "src-git luci https://github.com/coolsnowwolf/luci.git" >> "feeds.conf.default"
