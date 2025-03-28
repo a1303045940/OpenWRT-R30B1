76f5bf4c2b844198b4a4fdb0430d3ad3d87cf1e8
简 <39438074+JianJia2018@users.noreply.github.com>
2025-03-28 16:24:00 +0800

    build: 更新 MosDNS 插件版本
    - 将 luci-app-mosdns 的分支从 v5 切换到 v5-lua
    - 此更改旨在支持 Lua 版本的 MosDNS，可能会影响依赖关系和功能

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -42,7 +42,7 @@ rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
 
 #mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # #Open Clash
